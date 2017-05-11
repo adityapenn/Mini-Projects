fc4dfd95bf3ad2e24dcb67a260fdc440fac257cd
Aditya S. Pendyala <apendyal@stevens.edu>
2017-05-10 20:07:56 -0400

    Update extra_credit.py

--- Extra_Credit/extra_credit.py
+++ Extra_Credit/extra_credit.py
@@ -9,38 +9,34 @@ class KNN():
         self.labels = y
         self.neighbors = k
 
-        self.kneigh = NearestNeighbors(n_neighbors = 4, algorithm = 'kd_tree').fit(X)
+        self.kneigh = NearestNeighbors(n_neighbors = 4, algorithm = 'kd_tree').fit(X) #Fitting our data X to the nearest neighbors KD-Tree algorithm
 
-        if type(self.labels[1]) == object:
+        if type(self.labels[1]) == object: #Classifiers will have objects/strings as labels, as opposed to regressors which will have numerical values as labels
             typ = 'C'
         else:
-            typ = 'R'
+            typ = 'R' #The usage of 'typ' will be observed later in the program
 
-    def predict():
-        data_point = np.array([0, 1, 2, 2])
-        data1 = data.reshape(1,-1)
-        distances, indices = self.kneigh.kneighbors(data_point, n_neighbors = self.neighbors, metric = 'euclidean')
+        data_point = np.array([0, 1, 2, 2]) #input argument to the following function
+    def predict(data_point):
 
-        nbrs = NearestNeighbors(n_neighbors=k, algorithm='kd_tree').fit(X)
+        data1 = data_point.reshape(1,-1)
+        distances, indices = self.kneigh.kneighbors(data1, n_neighbors = self.neighbors, metric = 'euclidean') #Fitting our reshaped argument array to the k-neighbors algorithm
 
         for d in indices:
-            ind = Counter(self.labels[d])
-            lab = self.labels[d]
+            ind = Counter(self.labels[d]) #Dict with each label and number of times it occurs
 
-        if value == max(ind.values):
-            plabel = [sorted(key) for key, value in ind]
-        prob = max(ind.values)/self.neighbors
-        return(plabel[0], prob)
-
-        u = sum(lab)/len(lab)
-        for d2 in distances:
-                absolute = 0
-                absolute = absolute + abs(d2 - u)
-        avgd = absolute/k
-
-        if typ == 'C':
-            print(plabel[0], prob)
+        if typ == 'C': #For classifiers only
+            value = max(ind.values)
+            plabel = [sorted(key) for key, value in ind] #Get sorted labels
+            prob = max(ind.values)/self.neighbors #Finding posterior probability
+            return(plabel[0], prob)
         else:
+            lab = [self.labels[d] for d in indices]
+            u = sum(lab)/len(lab)
+            for d2 in distances:
+                    absolute = 0
+                    absolute = absolute + abs(d2 - u)
+            avgd = absolute/k
             print(u, avgd)
 
 df = pd.read_csv('breast-cancer-wisconsin.data.txt') #Data set taken from UCI's sample database, thanks to a Youtuber's tutorial video
@@ -50,4 +46,3 @@ x = df.values
 y = df.columns.values
 k = 3
 df2 = KNN(x, y, k)
-
